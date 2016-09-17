4ef1e8d87d99d31346b41ef020f59936c2b9e166
yuanjc <530072467@qq.com>
2016-09-17 14:40:32 +0800

    complete allocator

--- Allocator.h
+++ Allocator.h
@@ -3,6 +3,9 @@
 #include <cstddef>
 namespace TinySTL
 {
+
+class alloc; // allocate deallocate Impl
+
 template<typename T>
 class allocator
 {
@@ -14,29 +17,25 @@ public:
     typedef const T&    const_reference;
     typedef size_t      size_type;
     typedef ptrdiff_t   difference_type;
-
-};
-template<typename T, typename Alloc>
-class simple_alloc
-{
 public:
     static T* allocate(size_t n)
     {
-        return n == 0 ? nullptr : static_cast<T*>(Alloc::allocate(n * sizeof(T)));
+        return n == 0 ? nullptr : static_cast<T*>(alloc::allocate(n * sizeof(T)));
     }
     static T* allocate()
     {
-        return static_cast<T*>(Alloc::allocate(sizeof(T)));
+        return static_cast<T*>(alloc::allocate(sizeof(T)));
     }
     static void deallocate(T *p, size_t n)
     {
         if( n != 0)
-            Alloc::deallocate(p, n*sizeof(T));
+            alloc::deallocate(p, n*sizeof(T));
     }
     static void deallocate(T *p)
     {
-        Alloc::deallocate(p,sizeof(T));
+        alloc::deallocate(p,sizeof(T));
     }
+
 };
 }
 #endif // ALLOCATOR_H
