777e20da2648eaab2d855719923abf3626683805
yuanjc <530072467@qq.com>
2016-12-02 21:51:35 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,2 +1,9 @@
-#include<utility>
-#include<functional>
+#include <iostream>
+#include "Algorithm.h"
+int main()
+{
+    char str1[] = "abcdefgdefghijkl";
+    char str2[] = "defgh";
+    char* res = TinySTL::search(str1,str1+12,str2,str2+5);
+    std::cout<<res - str1 << std::endl;
+}
