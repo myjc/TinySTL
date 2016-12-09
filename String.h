02553b6beee8c5b4ff606f10ff4b56ad19024b9c
yuanjc <530072467@qq.com>
2016-12-09 21:54:33 +0800

    add

---
+++ String.h
@@ -0,0 +1,14 @@
+#ifndef TINYSTL_STRING_H
+#define TINYSTL_STRING_H
+#include "Algorithm.h"
+#include "Allocator.h"
+namespace TinySTL
+{
+template<typename CharType,typename Alloc = alloc>
+class BasicString
+{
+
+};
+}
+
+#endif // TINYSTL_STRING_H
