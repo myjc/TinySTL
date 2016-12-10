7a610945b3b1ac8a71864dd588b916bdae9d4110
yuanjc <530072467@qq.com>
2016-12-10 23:43:03 +0800

    update

--- String.h
+++ String.h
@@ -7,7 +7,17 @@ namespace TinySTL
 template<typename CharType,typename Alloc = alloc>
 class BasicString
 {
-
+public:
+    typedef CharType value_type;
+    typedef value_type* pointer;
+    typedef size_t  size_type;
+    typedef pointer iterator;
+public:
+    BasicString():data_(nullptr){}
+    BasicString(char* ptr):data_(ptr){}
+    BasicString(BasicString& str):data_(str.data_){}
+private:
+    pointer data_;
 };
 }
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>   
-#include "Algorithm.h"
+#include <string>
+using namespace std;
 int main()
 {
-    char str1[] = "abcdefgdefghijkl";
-    char str2[] = "defgh";
-    char* res = TinySTL::search(str1,str1+12,str2,str2+5);
-    std::cout<<res - str1 << std::endl;
+    string str;
+    str.c_str();
 }
