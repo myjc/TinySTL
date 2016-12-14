317dfead639471c99ab4ef80f58fba7768294d0b
yuanjc <530072467@qq.com>
2016-12-14 23:12:00 +0800

    none

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>   
-#include <string>
+#include <vector>
+#include "Algorithm.h"
+#include<algorithm>
 using namespace std;
+void display(int arr[],int length)
+{
+    for(int i = 0; i != length;++i)
+    {
+        cout << *(arr + i) <<" ";
+    }
+    cout << std::endl;
+}
+
 int main()
 {
-    string str;
-    str.c_str();
+    int arr[] = {9,8,7,6,5,4,3,2,1,10};
+    int length = sizeof(arr) / sizeof(int);
+    cout << length << endl;
+    display(arr,length);
+    std::push_heap(arr,arr+10);
+    display(arr,length);
+    std::pop_heap(arr,arr+10);
+    display(arr,length);
+    std::sort_heap(arr,arr+9);
+    display(arr,length);
+    std::make_heap(arr,arr+10);
+    display(arr,length);
 }
