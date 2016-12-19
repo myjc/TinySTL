19dd115a5b41ffa4381e8645e8ddeebe3d273913
yuanjc <530072467@qq.com>
2016-12-19 15:02:43 +0800

    fix todo

--- Uninitialize.h
+++ Uninitialize.h
@@ -3,29 +3,16 @@
 #include "TypeTraits.h"
 #include "Iterator.h"
 #include "Construct.h"
+#include "Algorithm.h"
 namespace TinySTL
 {
 
 /*********************************************************************/
 template<typename ForwardIterator,typename Size,typename T>
-inline ForwardIterator uninitialized_fill_n(ForwardIterator iter,Size n,const T& x)
-{
-    return _uninitialized_fill_n(iter,n,x,value_type(iter));
-}
-template<typename ForwardIterator,typename Size,typename T,typename T1>
-inline ForwardIterator _uninitialized_fill_n(ForwardIterator iter,Size n,const T& x,T*)
-{
-    typedef typename _type_traits<T1>::is_POD_type is_POD;
-    return _uninitialized_fill_n_aux(iter,n,x,is_POD());
-}
-
-/*
-template<typename ForwardIterator,typename Size,typename T>
 inline ForwardIterator _uninitialized_fill_n_aux(ForwardIterator iter,Size n,const T& x,_true_type)
 {
-    //todo :call fill_n()
+    TinySTL::fill_n(iter,n,x);
 }
-*/
 template<typename ForwardIterator,typename Size,typename T>
 inline ForwardIterator _uninitialized_fill_n_aux(ForwardIterator iter,Size n,const T& x,_false_type)
 {
@@ -37,32 +24,25 @@ inline ForwardIterator _uninitialized_fill_n_aux(ForwardIterator iter,Size n,con
     }
     return curr;
 }
-
+template<typename ForwardIterator,typename Size,typename T,typename T1>
+inline ForwardIterator _uninitialized_fill_n(ForwardIterator iter,Size n,const T& x,T*)
+{
+    typedef typename _type_traits<T1>::is_POD_type is_POD;
+    return _uninitialized_fill_n_aux(iter,n,x,is_POD());
+}
+template<typename ForwardIterator,typename Size,typename T>
+inline ForwardIterator uninitialized_fill_n(ForwardIterator iter,Size n,const T& x)
+{
+    return _uninitialized_fill_n(iter,n,x,value_type(iter));
+}
 /*********************************************************************/
-template<typename InputIterator, typename ForwardIterator>
-inline ForwardIterator uninitialized_copy(InputIterator first,InputIterator last, ForwardIterator dest)
-{
-    return _uninitialized_copy(first,last,dest,value_type(dest));
-
-}
-template<typename InputIterator, typename ForwardIterator,typename T>
-inline ForwardIterator _uninitialized_copy(InputIterator first,InputIterator last, ForwardIterator dest,T*)
-{
-    typedef typename _type_traits<T>::is_POD_type is_POD;
-    return _uninitialized_copy_aux(first,last,dest,is_POD());
-
-}
-
-
-
-/*
 template<typename InputIterator, typename ForwardIterator>
 inline ForwardIterator _uninitialized_copy_aux(InputIterator first,InputIterator last,
                                                ForwardIterator dest,_true_type)
 {
-//todo : call copy();
+    TinySTL::copy(first,last,dest);
 }
-*/
+
 template<typename InputIterator, typename ForwardIterator>
 inline ForwardIterator _uninitialized_copy_aux(InputIterator first,InputIterator last,
                                                ForwardIterator dest,_false_type)
@@ -75,19 +55,20 @@ inline ForwardIterator _uninitialized_copy_aux(InputIterator first,InputIterator
     return iter;
 
 }
+template<typename InputIterator, typename ForwardIterator,typename T>
+inline ForwardIterator _uninitialized_copy(InputIterator first,InputIterator last, ForwardIterator dest,T*)
+{
+    typedef typename _type_traits<T>::is_POD_type is_POD;
+    return _uninitialized_copy_aux(first,last,dest,is_POD());
 
+}
+template<typename InputIterator, typename ForwardIterator>
+inline ForwardIterator uninitialized_copy(InputIterator first,InputIterator last, ForwardIterator dest)
+{
+    return _uninitialized_copy(first,last,dest,value_type(dest));
+
+}
 /*********************************************************************/
-template<typename ForwardIterator,typename T>
-inline void uninitialized_fill(ForwardIterator first,ForwardIterator last, const T &x)
-{
-    _uninitialized_fill(first,last,x,value_type(first));
-}
-template<typename ForwardIterator,typename T,typename T1>
-inline void _uninitialized_fill(ForwardIterator first,ForwardIterator last, const T &x,T1*)
-{
-    typedef typename _type_traits<T1>::is_POD_type is_POD;
-    _uninitialized_fill_aux(first,last,x,is_POD());
-}
 template<typename ForwardIterator,typename T>
 inline void _uninitialized_fill_aux(ForwardIterator first,ForwardIterator last,
                                                const T &x,_false_type)
@@ -97,14 +78,23 @@ inline void _uninitialized_fill_aux(ForwardIterator first,ForwardIterator last,
         construct(&*first,x);
     }
 }
-/*
 template<typename ForwardIterator,typename T>
 inline void _uninitialized_fill_aux(ForwardIterator first,ForwardIterator last,
                                                const T &x,_true_type)
 {
-    todo : call fill()
+    fill(first,last,x);
 }
-*/
+template<typename ForwardIterator,typename T,typename T1>
+inline void _uninitialized_fill(ForwardIterator first,ForwardIterator last, const T &x,T1*)
+{
+    typedef typename _type_traits<T1>::is_POD_type is_POD;
+    _uninitialized_fill_aux(first,last,x,is_POD());
+}
+template<typename ForwardIterator,typename T>
+inline void uninitialized_fill(ForwardIterator first,ForwardIterator last, const T &x)
+{
+    _uninitialized_fill(first,last,x,value_type(first));
+}
 }//namespace TinySTL
 
 #endif // UNINITIALIZE_H
