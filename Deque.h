a7620a09d1e48b5081539437841c3ff4c1fcf18e
yuanjc <530072467@qq.com>
2017-03-20 16:46:56 +0800

    finish deque

--- Deque.h
+++ Deque.h
@@ -1,31 +1,39 @@
 #ifndef TINYSTL_DEQUE_H
 #define TINYSTL_DEQUE_H
+#include "Allocator.h"
 #include "Iterator.h"
+#include "Algorithm.h"
+#include "Uninitialize.h"
 namespace TinySTL
 {
-inline size_t dequeBuffSize(size_t n,size_t sz)
+inline size_t deque_buff_size(size_t n,size_t sz)
 {
     if( n != 0) return n;
     return sz < 512 ? 512/sz : 1;
 }
 
-template<typename T,size_t BuffSize>
+template<typename T,size_t BuffSize = 0>
 class DequeIterator:public iterator<random_access_iterator_tag,T>
 {
 public:
-    typedef typename DequeIterator self;
+    typedef typename iterator<random_access_iterator_tag,T>::value_type value_type;
+    typedef typename iterator<random_access_iterator_tag,T>::pointer pointer;
+    typedef typename iterator<random_access_iterator_tag,T>::difference_type difference_type;
+    typedef typename iterator<random_access_iterator_tag,T>::reference reference;
+    typedef typename iterator<random_access_iterator_tag,T>::iterator_category iterator_category;
+    typedef DequeIterator self;
     typedef value_type** map_pointer;
 private:
     value_type* cur_;
     value_type* first_;
     value_type* last_;
     map_pointer node_;
-    static size_t bufferSize(){ return dequeBuffSize(Buffsize,sizeof(T));}
+    static size_t buffer_size(){ return deque_buff_size(BuffSize,sizeof(T));}
     void setNode(map_pointer newNode)
     {
         node_ = newNode;
         first_ = *newNode;
-        last_ = first_ + bufferSize();
+        last_ = first_ + buffer_size();
     }
 
 public:
@@ -34,7 +42,7 @@ public:
     //distance of iterator
     difference_type operator-(const self& x) const
     {
-        return difference_type(bufferSize()) * (node_-x.node_-1) +(cur_ - first_)+(x.last_-x.cur_);
+        return difference_type(buffer_size()) * (node_-x.node_-1) +(cur_ - first_)+(x.last_-x.cur_);
     }
     self& operator++()
     {
@@ -62,7 +70,7 @@ public:
         --cur_;
         return *this;
     }
-    self operator--()
+    self operator--(int)
     {
         self tmp = *this;
         --*this;
@@ -71,7 +79,7 @@ public:
     self& operator+=(difference_type n)
     {
         difference_type offset = n +(cur_ - first_);
-        if(offset >= 0 && offset < bufferSize())
+        if(offset >= 0 && offset < buffer_size())
         {
             cur_ += n;
         }
@@ -79,11 +87,11 @@ public:
         {
             difference_type nodeOffset = 0;
             if(offset > 0)
-                nodeOffset = offset / bufferSize();
+                nodeOffset = offset / buffer_size();
             else
-                nodeOffset = (offset + 1)/bufferSize() - 1;
+                nodeOffset = (offset + 1)/buffer_size() - 1;
             setNode(node_ + nodeOffset);
-            cur_ = first_ + (offset - nodeOffset * bufferSize());
+            cur_ = first_ + (offset - nodeOffset * buffer_size());
         }
         return *this;
     }
@@ -117,6 +125,66 @@ public:
     {
         return (node_ == iter.node_) ? (cur_ < iter.cur_) : (node_ < iter.node_);
     }
+};
+template<typename T,typename Alloc = alloc,size_t BuffSize = 0>
+class Deque
+{
+public:
+    typedef T value_type;
+    typedef value_type* pointer;
+    typedef value_type& reference;
+    typedef size_t size_type;
+    typedef DequeIterator<T,BuffSize> iterator;
+    typedef typename iterator::difference_type difference_type;
+private:
+    typedef pointer* map_pointer;
+    typedef allocator<value_type,Alloc> data_allocator;
+    typedef allocator<pointer,Alloc> map_allocator;
+    data_allocator data_alloc_;
+    map_allocator map_alloc_;
+    iterator start_;
+    iterator finish_;
+    map_pointer map_;
+    size_type map_size_;
+public:
+    Deque();
+    Deque(int n ,const value_type& value);
+    Deque(const Deque& deq);
+    Deque(Deque&& deq);
+    Deque& operator=(const Deque&);
+    Deque& operator=(Deque&&);
+    ~Deque(){ clear();}
+
+public:
+    iterator begin() { return start_;}
+    iterator end() { return finish_;}
+    reference operator[] (difference_type n){ return start_[n];}
+    reference front(){ return *start_;}
+    reference back() { return *(finish_ -1);}
+    size_type size() const { return finish_ - start_ ;}
+    bool empty() { return start_ == finish_;}
+    void push_front(const value_type& value);
+    void push_back(const value_type& value);
+    void pop_front();
+    void pop_back();
+    void clear();
+    iterator erase(iterator postion);
+    iterator insert(iterator position,const value_type& value);
+private:
+    void fill_initialize(size_type n, const value_type& value);
+    void create_map_and_nodes(size_type n);
+    size_type buffer_size(){ return iterator::buffer_size();}
+    static size_type initial_map_size(){ return 8;} //minimum map size;
+    pointer allocate_node(){ return data_alloc_.allocate(buffer_size());}
+    void push_back_aux(const value_type&);
+    void push_front_aux(const value_type&);
+    void pop_front_aux();
+    void pop_back_aux();
+    void insert_aux(iterator position, const value_type &value);
+    void reserve_map_at_back(size_type nodes_to_add = 1);
+    void reserve_map_at_front(size_type nodes_to_add = 1);
+    void reallocate_map(size_type nodes_to_add,bool add_at_front);
+
 };
 }
 #endif // TINYSTL_DEQUE_H
---
+++ Impl/Deque.cpp
@@ -0,0 +1,257 @@
+#include "Deque.h"
+namespace TinySTL
+{
+template<typename T,typename Alloc,size_t Buffsize>
+void Deque<T,Alloc,Buffsize>::create_map_and_nodes(size_type num_element)
+{
+    size_type num_nodes = num_element / buffer_size() +1;
+    map_size_ = TinySTL::max(initial_map_size(),num_nodes);
+    map_ = map_alloc_.allocate(map_size_);
+    map_pointer nstart = map_+(map_size_ - num_nodes)/2;
+    map_pointer nfinish = nstart + num_nodes -1;
+    map_pointer cur = nullptr;
+    for(cur = nstart; cur < nfinish; ++cur)
+    {
+        *cur = allocate_node();
+    }
+    start_.setNode(nstart);
+    finish_.setNode(nfinish);
+    start_.cur_ = start_.first_;
+    finish_.cur_ = finish_.first_ + num_element % buffer_size();
+}
+template<typename T,typename Alloc,size_t Buffsize>
+void Deque<T,Alloc,Buffsize>::fill_initialize(size_type n, const value_type &value)
+{
+    create_map_and_nodes(n);
+    map_pointer cur = nullptr;
+    for(cur = start_.node_; cur < finish_.node_; ++cur)
+    {
+        TinySTL::uninitialized_fill(*cur,*cur+buffer_size(),value);
+    }
+    uninitialized_fill(finish_.first_, finish_.cur_, value);
+}
+template<typename T,typename Alloc,size_t Buffsize>
+Deque<T,Alloc,Buffsize>::Deque(int n, const value_type& value)
+{
+    fill_initialize(n,value);
+}
+template<typename T,typename Alloc,size_t Buffsize>
+void Deque<T,Alloc,Buffsize>::push_back(const value_type& value)
+{
+    if(finish_.cur_ != finish_.last_ -1)
+    {
+        construct(finish_.cur_,value);
+        ++finish_.cur_;
+    }
+    else
+    {
+        push_back_aux(value);
+    }
+}
+template<typename T,typename Alloc,size_t Buffsize>
+void Deque<T,Alloc,Buffsize>::reallocate_map(size_type nodes_to_add,bool add_at_front)
+{
+    size_type old_nodes_num = finish_.node_ - start_.node_ +1;
+    size_type new_nodes_num = old_nodes_num + nodes_to_add;
+    map_pointer new_nstart = nullptr;
+    if(map_size_ > 2 * new_nodes_num)
+    {
+        new_nstart = map_ + (map_size_ - new_nodes_num) /2
+                +(add_at_front ? nodes_to_add : 0);
+        if( new_nstart < start_.node_)
+            copy(start_.node_,finish_.node_ +1,new_nstart);
+        else
+            copy_backward(start_.node_,finish_.node_ +1,new_nstart + old_nodes_num);
+    }
+    else
+    {
+        size_type new_map_size = map_size_ + max(map_size_,nodes_to_add)+2;
+        map_pointer new_map = map_alloc_.allocate(new_map_size);
+        new_nstart = new_map + (new_map_size - new_nodes_num) /2 +
+                (add_at_front ? nodes_to_add : 0);
+        copy(start_.node_,finish_.node_ +1, new_nstart);
+        map_alloc_.deallocate(map_,map_size_);
+        map_ = new_map;
+        map_size_ = new_map_size;
+    }
+    start_.setNode(new_nstart);
+    finish_.setNode(new_nstart + old_nodes_num -1);
+}
+template<typename T,typename Alloc,size_t Buffsize>
+void Deque<T,Alloc,Buffsize>::reserve_map_at_back(size_type nodes_to_add)
+{
+    if(nodes_to_add > map_size_ - (finish_.node_ - map_ + 1))
+        reallocate_map(nodes_to_add,false);
+}
+template<typename T,typename Alloc,size_t Buffsize>
+void Deque<T,Alloc,Buffsize>::reserve_map_at_front(size_type nodes_to_add)
+{
+    if(nodes_to_add > start_.node_ - map_)
+        reallocate_map(nodes_to_add,true);
+}
+template<typename T,typename Alloc,size_t Buffsize>
+void Deque<T,Alloc,Buffsize>::push_back_aux(const value_type& value)
+{
+    value_type value_copy = value;
+    reserve_map_at_back();
+    *(finish_.node_ + 1) = allocate_node();
+    construct(finish_.cur_,value_copy);
+    finish_.setNode(finish_.node_ +1);
+    finish_.cur_ = finish_.first_;
+}
+template<typename T,typename Alloc,size_t Buffsize>
+void Deque<T,Alloc,Buffsize>::push_front(const value_type& value)
+{
+    if(start_.cur_ != start_.first_)
+    {
+        construct(start_.cur_ - 1,value);
+        --start_.cur_;
+    }
+    else
+    {
+        push_front_aux(value);
+    }
+}
+template<typename T,typename Alloc,size_t Buffsize>
+void Deque<T,Alloc,Buffsize>::push_front_aux(const value_type& value)
+{
+    reserve_map_at_front();
+    *(start_.node_ -1) = allocate_node();
+    start_.setNode(start_.node_ -1);
+    start_.cur_ = start_.last_ - 1;
+    construct(start_.cur_,value);
+}
+template<typename T,typename Alloc,size_t Buffsize>
+void Deque<T,Alloc,Buffsize>::pop_front()
+{
+    if(start_.cur_ != start_.last_ -1)
+    {
+        destroy(start_.cur_);
+        ++start_.cur_;
+    }
+    else
+        pop_front_aux();
+}
+template<typename T,typename Alloc,size_t Buffsize>
+void Deque<T,Alloc,Buffsize>::pop_back()
+{
+    if(finish_.cur_ != finish_.first_)
+    {
+        --finish_.cur_;
+        destroy(finish_.cur_);
+    }
+    else
+        pop_back_aux();
+
+}
+template<typename T,typename Alloc,size_t Buffsize>
+void Deque<T,Alloc,Buffsize>::pop_front_aux()
+{
+    destroy(start_.cur_);
+    if(start_.node_ != finish_.node_) //always true?
+    {
+        data_alloc_.deallocate(start_.first_, buffer_size());
+        map_alloc_.destroy(start_.node_);
+        start_.setNode(start_.node_ + 1);
+        start_.cur_ = start_.first_;
+    }
+}
+template<typename T,typename Alloc,size_t Buffsize>
+void Deque<T,Alloc,Buffsize>::pop_back_aux()
+{
+    data_alloc_.deallocate(finish_.first_,buffer_size());
+    if(finish_.node_ != start_.node_)//if not true, then it is an empty deque,nothing to do
+    {
+        finish_.setNode(finish_.node_ -1);
+        finish_.cur_ = finish_.last_ -1;
+        destroy(finish_.cur_);
+    }
+}
+template<typename T,typename Alloc,size_t Buffsize>
+void Deque<T,Alloc,Buffsize>::clear()
+{
+    for(map_pointer node = start_.node_ +1; node < finish_.node_; ++node)
+    {
+        destroy(*node,*node + buffer_size());
+        data_alloc_.deallocate(*node,buffer_size());
+    }
+    if(start_.node_ != finish_.node_)
+    {
+        destroy(start_.cur_,start_.last_);
+        destroy(finish_.first_, finish_.cur_);
+        data_alloc_.deallocate(finish_.first_,buffer_size());
+    }
+    else
+    {
+        destroy(start_.cur_, finish_.cur_);
+    }
+    finish_ = start_;
+}
+template<typename T,typename Alloc,size_t Buffsize>
+Deque<T,Alloc,Buffsize>::iterator Deque<T,Alloc,Buffsize>::erase(iterator pos)
+{
+    iterator next = pos;
+    ++next;
+    difference_type index = pos - start_;
+    if(index < size() /2)
+    {
+        copy_backward(start_,pos,next);
+        pop_front();
+    }
+    else
+    {
+        copy(next,finish_,pos);
+        pop_back();
+    }
+    return start_ + index;
+}
+template<typename T,typename Alloc,size_t Buffsize>
+Deque<T,Alloc,Buffsize>::iterator
+Deque<T,Alloc,Buffsize>::insert(iterator pos,const value_type& val)
+{
+    if(pos == start_)
+    {
+        push_front(val);
+        return start_;
+    }
+    else if(pos == finish_)
+    {
+        push_back(val);
+        iterator tmp = finish_;
+        return --tmp;
+    }
+    else
+    {
+        return insert_aux(pos,val);
+    }
+}
+template<typename T,typename Alloc,size_t Buffsize>
+Deque<T,Alloc,Buffsize>::iterator
+Deque<T,Alloc,Buffsize>::insert_aux(iterator pos,const value_type& val)
+{
+    difference_type index = pos - start_;
+    if(index < size() /2)
+    {
+        push_front(front());
+        iterator front1 = start_;
+        ++front1;
+        iterator front2 = front1;
+        ++front2;
+        iterator pos = start_ + index;
+        ++pos;
+        copy(front2,pos,front1);
+    }
+    else
+    {
+        push_back(back());
+        iterator back1 = finish_;
+        --back1;
+        iterator back2 = back1;
+        --back2;
+        pos = start_ + index;
+        copy_backward(pos,back2,back1);
+    }
+    *pos = x;
+    return pos;
+}
+}//end of namespace
